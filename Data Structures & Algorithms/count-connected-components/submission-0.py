8920a7e7b4e582d3144079a04b93437a1955eed8
Kei <66120473+kei073@users.noreply.github.com>
2026-04-11 16:45:16 +0900

    Add: count-connected-components - submission-0

---
+++ Data Structures & Algorithms/count-connected-components/submission-0.py
@@ -0,0 +1,30 @@
+class UnionFind:
+    def __init__(self, n: int) -> None:
+        self.n = n
+        self.par = [x for x in range(n)]
+    
+    def find(self, x: int) -> int:
+        if x == self.par[x]:
+            return x
+        self.par[x] = self.find(self.par[x])
+        return self.par[x]
+
+    def union(self, x: int, y: int) -> None:
+        x = self.find(x)
+        y = self.find(y)
+        
+        self.par[x] = y
+        
+
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        uf = UnionFind(n)
+        for x, y in edges:
+            uf.union(x, y)
+        
+        root_set = set()
+        for x in range(n):
+            root_set.add(uf.find(x))
+        
+        return len(root_set)
+
